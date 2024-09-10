b6d49343b7d59f0fef3801c676e725d3f1466bf3
Aero <aero@gmail.com>
2024-09-10 17:59:14 +0500

    Initial Commit

---
+++ 1stdz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stdz
+//
+//  Created by vv 77 on 10/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
